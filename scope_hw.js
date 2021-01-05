a33193c3ad3ce1bbef46066ab34c48855e72c3a2
Andrew Wood <andrewwood90@hotmail.com>
2021-01-05 22:53:04 +0000

    initial commit

---
+++ scope_hw.js
@@ -0,0 +1,203 @@
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Library',
+//     weapon: 'Rope'
+// };
+
+// const declareMurderer = function() {
+//     return `The murderer is ${scenario.murderer}.`;
+// }
+
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+
+/* 1 the murderer is Miss Scarlet, because she is the 'murderer' value in the scenario object, 
+which is called in the declareMurderer function. */
+
+// const murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//     murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//     return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+/* 2 The murderer is Professor Plum- the const variable can't be given a new value. */
+
+
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+/*3 the 'first verdict' murderer is Mrs Peacock, who is assigned to the murderer value by firstVerdict.
+However, for the second verdict the murderer is Prof Plum. */
+
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+
+/* 4 Suspect three will be Mrs Peacock. suspectThree is reassigned to Colonel Mustard, but only within
+the scope of the declareAllSupsects function (as the keyword let was used)
+*/
+
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Kitchen',
+    weapon: 'Candle Stick'
+  };
+  
+  const changeWeapon = function(newWeapon) {
+    scenario.weapon = newWeapon;
+  }
+  
+  const declareWeapon = function() {
+    return `The weapon is the ${scenario.weapon}.`;
+  }
+  
+  changeWeapon('Revolver');
+  const verdict = declareWeapon();
+  console.log(verdict);
+
+/* 5 Miss Scarlet used the revolver. The const 'scenario's weapon was changed with the 
+changeWeapon function (like the song title was changed yesterday)
+*/
+
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+
+/* 6 Due to the plot twist function having global scope, the final murderer is Mrs White
+*/
+
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+/*7 The Murderer is Mrs Scarlet.Though plotTwist (Cl Mustard) is called after unexpectedOutcome,
+its scope is only within that function. 
+*/
+
+const scenario = {
+    murderer: 'Mrs. Peacock',
+    room: 'Conservatory',
+    weapon: 'Lead Pipe'
+  };
+  
+  const changeScenario = function() {
+    scenario.murderer = 'Mrs. Peacock';
+    scenario.room = 'Dining Room';
+  
+    const plotTwist = function(room) {
+      if (scenario.room === room) {
+        scenario.murderer = 'Colonel Mustard';
+      }
+  
+      const unexpectedOutcome = function(murderer) {
+        if (scenario.murderer === murderer) {
+          scenario.weapon = 'Candle Stick';
+        }
+      }
+  
+      unexpectedOutcome('Colonel Mustard');
+    }
+  
+    plotTwist('Dining Room');
+  }
+  
+  const declareWeapon = function() {
+    return `The weapon is ${scenario.weapon}.`
+  }
+  
+  changeScenario();
+  const verdict = declareWeapon();
+  console.log(verdict);
+
+
+/* 8 Colonel Mustard with the candlestick! UnexpectedOutcome and plotTwist are both successfully
+called, so the original scenario attributes will change.
+*/
+
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+
+/* The murderer is Cl Mustard. The change to Mrs Peacock takes place within a block, and is done by a 
+let keyword, so will not affect the value outside that
+ */
+
+
+
